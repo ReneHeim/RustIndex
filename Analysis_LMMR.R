faeff625e9eb4711b4d6b4de181f663224d0ed0a
ReneHeim <rene.heim@hdr.mq.edu.au>
2018-01-18 09:50:46 +0100

    Version 0.22
    Edits:
    
    - Removed output folder
    - Moved feature.rds to /data
    - Created all analysis output (Figure 2 Spectra and Boxplots, Table. X and sinked test data output as basical information for article confusion matrix)
    
    Upcoming
    
    - Beautify script
      + Wrap sections in loops and      functions
      + Define clear sections
      + Generalize sections
      + (When submitting this manuscript it could be a major selling point that people can use this script to create their own index!)

--- Analysis_LMMR.R
+++ Analysis_LMMR.R
@@ -15,7 +15,7 @@
 
 # 1. Install and Load Packages ----------------------------------------------------------------------
 
-# install.packages(c("devtools","cowplot", "gdata", "glmulti", "hsdar", "plyr",
+# install.packages(c("cowplot", "gdata", "glmulti", "hsdar", "plyr",
 #                    "PresenceAbsence", "prospectr", "rJava", "tidyverse",
 #                    "VSURF", "reshape2", "caret"))
 
@@ -37,9 +37,6 @@ library(reshape2)
 library(caret)
 library(devtools)
 
-devtools::install_github("sachsmc/plotROC")
-library(plotROC)
-
 
 # 2. Loading Functions and set project structure-----------------------------------------------------
 
@@ -48,9 +45,9 @@ source('R/20170601_FUN_exportVSURF.R')
 source('R/20171224_FUN_raw2speclibhsdar.R')
 source('R/20171224_FUN_prepggwide2long.R')
 
--dir.create('data', FALSE, FALSE) # Contains original data only (Do not modify!)
--dir.create('R', FALSE, FALSE) # Contains functions
--dir.create('output', FALSE, FALSE) # Code generated output
+dir.create('data', FALSE, FALSE) # Contains original data only (Do not modify!)
+dir.create('R', FALSE, FALSE) # Contains functions
+dir.create('output', FALSE, FALSE) # Code generated output
 
 # 3. Loading and Preparing Data ---------------------------------------------------------------------
 
@@ -71,7 +68,7 @@ data[names(data)[-1]] <-
 
 # 4. Selecting Subset of Relevant Wavebands ---------------------------------------------------------
 
-set.seed(20180111)
+#set.seed(20180111)
 
 # A) 10x VSURF Feature Selection (Runs ~30 hours)
 
@@ -102,7 +99,7 @@ VSURF.selection <- unlist(band.vectors) #Unlist list to create a vector containi
 VSURF.selection <- sort(unique(VSURF.selection)) #Only select unique bands from vector and sort
 
 # 5. Model Selection to find 4 best bands to explain seperation between healthy and treated-----------
-
+set.seed(20180117)
 multi.model <- glmulti(y=names(data)[1],xr=paste0('X',VSURF.selection),data,maxsize=4,level = 1, family=binomial)
 
 # 5.1 glmulti does not provide model coefficients, therefore add another logistic regression to
@@ -110,8 +107,8 @@ multi.model <- glmulti(y=names(data)[1],xr=paste0('X',VSURF.selection),data,maxs
 
 model.1 <- glm(as.formula(summary(multi.model)$bestmodel),data,family=binomial)
 
-saveRDS(model.1, 'output/LMMRmodel.RDS')
-model.1 <- readRDS(file = 'output/LMMRmodel.RDS')
+#saveRDS(model.1, 'output/LMMRmodel.RDS')
+#model.1 <- readRDS(file = 'output/LMMRmodel.RDS')
 
 # 5.2 Build LMMR complex equation
 
@@ -178,46 +175,76 @@ inTrain <- createDataPartition(y = index.df$Type, p = .75, list = FALSE)
 Train.75 <- index.df[inTrain,]
 Test.25 <- index.df[-inTrain,]
 
-ctrl <- trainControl(method = "repeatedcv",repeats = 10, classProbs = TRUE,summaryFunction = twoClassSummary)
+ctrl <- trainControl(method = "boot",
+                     number = 1000, 
+                     classProbs = FALSE, 
+                     savePredictions = TRUE)
 
-# PRI
-PRI.model <- train(Type ~ PRI, data = Train.75, method = "glm", trControl = ctrl, metric = 'Kappa')
+# PRI Training
+PRI.model <- train(Type ~ PRI, data = Train.75, method = "glm", trControl = ctrl, metric = c('Kappa'))
 summary(PRI.model)# estimates
+confusionMatrix(PRI.model)
+PRIvalues <- PRI.model$finalModel$fitted.values
+PRI.model$pred
+# PRI Testing
 PRI.pred <- predict(PRI.model, newdata = Test.25)
-summary(PRI.pred)
 
+sink("output/PRITest25.txt", append=FALSE, split=FALSE)
+confusionMatrix(data = PRI.pred, Test.25$Type)
+sink()
 confmat.PRI <- confusionMatrix(data = PRI.pred, Test.25$Type)
 
-# MCARI
+
+# MCARI Training
 MCARI.model <- train(Type ~ MCARI, data = Train.75, method = "glm", trControl = ctrl, metric = c("Kappa"))
-MCARI.model
+summary(MCARI.model)
+confusionMatrix(MCARI.model)
+MCARIvalues <- MCARI.model$finalModel$fitted.values
+# MCARI Test
 MCARI.pred <- predict(MCARI.model, newdata = Test.25)
+
+sink("output/MCARITest25.txt", append=FALSE, split=FALSE)
+confusionMatrix(data = MCARI.pred, Test.25$Type)
+sink()
 confmat.MCARI <- confusionMatrix(data = MCARI.pred, Test.25$Type)
 
-# NBNDVI
+# NBNDVI Training
 NBNDVI.model <- train(Type ~ NBNDVI, data = Train.75, method = "glm", trControl = ctrl, metric = c("Kappa"))
-NBNDVI.model
+summary(NBNDVI.model)
+confusionMatrix(NBNDVI.model)
+NBNDVIvalues <- NBNDVI.model$finalModel$fitted.values
+# NBNDVI Test
 NBNDVI.pred <- predict(NBNDVI.model, newdata = Test.25)
+
+sink("output/NBNDVITest25.txt", append=FALSE, split=FALSE)
+confusionMatrix(data = NBNDVI.pred, Test.25$Type)
+sink()
 confmat.NBNDVI <- confusionMatrix(data = NBNDVI.pred, Test.25$Type)
 
-# LMMR
+# LMMR Training
 LMMR.model <- train(Type ~ LMMR, data = Train.75, method = "glm", trControl = ctrl, metric = c("Kappa"))
-LMMR.model
+summary(LMMR.model)
+confusionMatrix(LMMR.model)
+LMMRvalues <- LMMR.model$finalModel$fitted.values
+# LMMR Test
 LMMR.pred <- predict(LMMR.model, newdata = Test.25)
+
+sink("output/LMMRTest25.txt", append=FALSE, split=FALSE)
+confusionMatrix(data = LMMR.pred, Test.25$Type)
+sink()
 confmat.LMMR <- confusionMatrix(data = LMMR.pred, Test.25$Type)
+# E) Create df from training fitted values
 
-confmat.LMMR$overall[2]
-# E) Create Figure 1 - Training data/ Model evaluation (plotROC pkg)
+train.res.df <- as.data.frame(Train.75$Type)
+train.res.df$PRIval <- PRIvalues
+train.res.df$MCARIval <- MCARIvalues
+train.res.df$NBNDVIval <- NBNDVIvalues
+train.res.df$LMMRval <- LMMRvalues
 
-long.Train75 <- melt_roc(Train.75, "Type", c("PRI", "MCARI", "NBNDVI", "LMMR"))
-names(long.Train75) <- c('Type', 'Value', 'Index')
-
-ggplot(long.Train75, aes(d = Type, m = Value, color = Index)) + 
-  geom_roc(n.cuts = 50, labels = FALSE, aes(shape=Index)) + 
-  style_roc(xlab = "1 - Specificity", ylab="Sensitivity")
+names(train.res.df) <- c('Type', 'PRI', 'MCARI', 'NBNDVI', 'LMMR')
 
 
-# F) Create Table 1 - Test data accuracy metrics
+# F) Create suppl. Table...Table 1 based on test data confusion matrices (sinked objects)
 
 OA <- round(c('PRI'= confmat.PRI$overall[1], 
         'MCARI'= confmat.MCARI$overall[1], 
@@ -239,21 +266,52 @@ Specificity <- round(c('PRI'= confmat.PRI$byClass[2],
                  'NBNDVI'= confmat.NBNDVI$byClass[2],
                  'LMMR'= confmat.LMMR$byClass[2]), 3)*100
 
-testresult.df <- as.data.frame(cbind(OA, Kappa, Sensitivity, Specificity))
+TableSupp <- as.data.frame(cbind(OA, Kappa, Sensitivity, Specificity))
 
-names(testresult.df) <- c('OA[%]', 'Kappa[%]', 'Sensitivity[%]', 'Specificity[%]')
-row.names(testresult.df) <- c('PRI', 'MCARI', 'NBNDVI', 'LMMR')
+names(TableSupp) <- c('OA[%]', 'Kappa[%]', 'Sensitivity[%]', 'Specificity[%]')
+row.names(TableSupp) <- c('PRI', 'MCARI', 'NBNDVI', 'LMMR')
+
+write.csv(Table1, 'output/Table1.testaccumetrics.csv')
+
 # 7. Visualize Results ------------------------------------------------------------------------------
+indi <- names(train.res.df[, 2:length(train.res.df)])
+plot_list <- list()
+for (i in indi) {
+
+plot_list[[i]] <- ggplot(train.res.df, aes_string(train.res.df$Type, train.res.df[, i], colour = train.res.df$Type))+
+  geom_jitter(aes(shape=Type),height = 0)+
+  geom_boxplot(colour = 'black', alpha = 0.5, outlier.alpha = 0)+
+  theme(axis.text = element_text(size = 16),
+        axis.title = element_text(size = 16, face = "bold"),
+        legend.position = "none")+
+        xlab(label = paste(i))+
+        ylab(label = 'Index Value')+
+        ylim(0, 1)
+}
 
 
+p1 <- plot_grid(
+  plot_list[[4]],
+  plot_list[[2]],
+  plot_list[[3]],
+  plot_list[[1]],
+  labels = c("a", 'b', 'c', 'd'),
+  ncol = 4,
+  nrow = 1)
 
+p1
+  
+  
+# Spectra plots
 
 # B) Plot spectra and show final most important wavebands
-
+    
 spectra.gg <- prep.gg(tospectra) # Transforms wide to long for ggplot2 readibility
+  
+# Subplot E (Spectra including spectral regions and best bands)
 
 bands4gg <-as.numeric(gsub('X', '', best.bands))
-
+  
 pspec <- ggplot(spectra.gg, aes(Wavelength, Reflectance, colour = Type)) +
   geom_line(aes(linetype=Type), size = 1)+
   geom_point(aes(shape=Type), size = 2)+
@@ -346,30 +404,54 @@ pspec <- ggplot(spectra.gg, aes(Wavelength, Reflectance, colour = Type)) +
   theme(legend.position = c(.90, .88), legend.title = element_blank(), legend.background = element_blank())
 
 pspec
+  
+# C) Get bar/jitter plots from 7A and modify to combine with 7B
 
-# C) Build Zoomed version
+p5 <- plot_list[[2]]#MCARI
+p5 <- p5+
+  theme(#axis.title.x=element_blank(),
+      axis.text.x=element_blank(),
+      axis.ticks.x=element_blank(),
+      axis.title.y=element_blank())
 
+p6 <- plot_list[[3]]#NBNDVI
+p6 <- p6+
+    theme(#axis.title.x=element_blank(),
+      axis.text.x=element_blank(),
+      axis.ticks.x=element_blank(),
+      axis.title.y=element_blank())
+
+p7 <- plot_list[[4]]#LMMR
+p7 <- p7+
+    theme(#axis.title.x=element_blank(),
+      axis.text.x=element_blank(),
+      axis.ticks.x=element_blank(),
+      axis.title.y=element_blank())
+
+p9 <- plot_list[[1]]#PRI
+p9 <- p9+
+    theme(#axis.title.x=element_blank(),
+      axis.text.x=element_blank(),
+      axis.ticks.x=element_blank())+
+      labs(x = "PRI", y="Disease Prob.")
+  
 # D) Build Figure 2 (Figure 1 was designed outside of R using photographs and Inkscape)
-
+    
 plot.res <- ggdraw() +
-  draw_plot(p9, x = 0, y = .5, width = .2, height = .5) +
-  draw_plot(p8, x = 0.2, y = .5, width = .2, height = .5) +
-  draw_plot(p5, x = .4, y = .5, width = .2, height = .5) +
-  draw_plot(p6, x = .6, y = .5, width = .2, height = .5) +
-  draw_plot(p7, x = .8, y = .5, width = .2, height = .5) +
-  draw_plot(pspec, x = 0, y = 0, width = 1, height = 0.5) +
-  draw_plot_label(label = c("A", "B", "C", "D", "E", "F"), size = 12,
-                  x = c(0, 0.2, 0.4, 0.6,0.8, 0), y = c(1, 1, 1, 1, 1, 0.5))
+    draw_plot(p9, x = 0, y = .5, width = .25, height = .5) +
+    draw_plot(p5, x = .25, y = .5, width = .25, height = .5) +
+    draw_plot(p6, x = .5, y = .5, width = .25, height = .5) +
+    draw_plot(p7, x = .75, y = .5, width = .25, height = .5) +
+      draw_plot(pspec, x = 0, y = 0, width = 1, height = 0.5) +
+        draw_plot_label(label = c("A", "B", "C", "D", "E"), size = 12,
+                        x = c(0, 0.25, 0.5, 0.75, 0), y = c(1, 1, 1, 1, 0.5))
+  
 plot.res
-
-ggsave(
-  "output/20180108Results.png",
-  plot = plot.res,
-  width = 40,
-  height = 20,
-  units = "cm",
-  dpi = 400
-)
-
-
-
+  
+ggsave("output/Figure2.boxspectra.png",
+    plot = plot.res,
+    width = 40,
+    height = 20,
+    units = "cm",
+    dpi = 400
+  )
